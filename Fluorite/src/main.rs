60ced7f11ff8d888fc002c0965bf1241b588f5a2
phoenix fire wingz <luke.a.shore@proton.me>
2025-01-03 03:28:28 +0000

    Fluorite: change main to no LibCrystalMatrix API

--- Fluorite/src/main.rs
+++ Fluorite/src/main.rs
@@ -1,26 +1,29 @@
-use lib_crystal_matrix::net::packat::Packet;
-use lib_crystal_matrix::{close_window, open_window, pump_window};
+use libcrystalmatrix::{open_window};
 use std::sync::{Arc, Mutex};
+use libcrystalmatrix::libprotocol::Packet;
 
 fn main() {
     let should_close = Arc::new(Mutex::new(false));
     let should_close_clone = Arc::clone(&should_close);
-    open_window(Some("Flourite".to_string()), 720, 1280, move |packet| {
+
+    let client = open_window(Some("Flourite".to_string()),1280/2 ,720/2,move |packet| {
         match packet {
             Packet::Closed => {
                 let mut should_close = should_close_clone.lock().unwrap();
-                *should_close = true; // Update the shared value
+                *should_close = true;
             }
             _ => {}
         }
         None
-    });
+    }).expect("Failed to open window");
+
     loop {
         if *should_close.lock().unwrap() {
             break;
         }
-        pump_window();
+        client.pump_window();
     }
-    close_window();
+
+    client.close_window();
 }
 
